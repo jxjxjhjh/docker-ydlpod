49a229d51646a45a699a36b4084375cf650b1de7
JXJXJHJH <22759318+jxjxjhjh@users.noreply.github.com>
2021-04-24 14:54:33 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@ python -m http.server 8001
 
 while :
 do
-    timeout 300 ydl_podcast config.yaml
+    timeout 600 ydl_podcast config.yaml
+    echo $(date "+%Y-%m-%d %H:%M:%S") >> log.log
+    pip install --upgrade youtube-dl
     sleep 86400
 done
